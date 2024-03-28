7c303f940856f2525138e5fa848f3f7bdfda1e32
Tudor <tudor.dascalescu@mincode.ro>
2024-03-28 13:37:31 +0200

    Python bot

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # app_account_bot
+
+Prerequisites:
+
+- Python 3.X
+
+
+Steps:
+
+1. Run local replica and client app
+2. Create the first 3 system users accounts and another system account for you if needed
+3. Go inside this repo root
+4. Run `pip install -r requirements.txt` or `pip3 install -r requirements.txt`
+5. Run `python development.py` or `python3 development.py`
+6. Wait
+
+Keep in mind that this script does not save the created identities. You will not be able to access or export them
---
+++ development.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
+from time import sleep
+import requests
+
+
+# Set options for not prompting DevTools information
+options = Options()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+print("Generating accounts")
+index = 6
+driver = webdriver.Chrome(options=options)
+driver.get("http://localhost:8080/#/public/login")
+sleep(6)
+while index < 9:
+    sleep(2)
+    driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div/div[2]/div/button[2]').click() #click on Log In with test identity
+    sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="react-portal-modal-container"]/div/div/div/div/div[2]/button[1]').click() #click on identities
+    sleep(1)
+    driver.find_element(By.ID, 'title').send_keys(f'test{index}') #enter identity name in input
+    sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="react-portal-modal-container"]/div/div/div/div/div[1]/div[4]/button').click() #click on + button
+    sleep(1)
+    identity_list = driver.find_element(By.XPATH, '//*[@id="react-portal-modal-container"]/div/div/div/div/div[1]/div[3]/ul') #get identity list
+    items = identity_list.find_elements(By.TAG_NAME, "li") # extract all identities
+
+    driver.find_element(By.XPATH, f'//*[@id="react-portal-modal-container"]/div/div/div/div/div[1]/div[3]/ul/li[{len(items)}]').click() #select last created identity
+    sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="react-portal-modal-container"]/div/div/div/div/div[2]/button[2]').click() #click on consent
+    sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="react-portal-modal-container"]/div/div/div/div/div[2]/button[2]').click() #click on log in
+    sleep(3)
+    driver.find_element(By.ID, 'username').send_keys(f'test{index}') #enter username
+    driver.find_element(By.ID, 'fullname').send_keys(f'TEST{index}') #enter fullname
+    sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div/div/div[2]/div/form/button').click() #click on create account
+    sleep(5)
+    driver.find_element(By.XPATH, '//*[@id="react-joyride-step-0"]/div/div/div[1]/div[2]/div/button').click() #click on skip button
+    sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/nav/div[2]/li[2]').click() #click on avatar button
+    sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/aside/div/div[2]/div[2]/div/div/button[1]').click() #click on log out
+    index+=1
+
+print('clicked')
+driver.quit()
